97a5fbfc829f566c95317e4a7f42735581d7343a
Turchikz <saitov.artur96@mail.ru>
2024-04-24 17:30:52 +0500

    First commit 24.04.2024

---
+++ reestr_gks/reestr_list/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReestrListConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "reestr_list"
---
+++ start.bat
@@ -0,0 +1,2 @@
+CALL .\venv\Scripts\Activate.ps1
+python .\reestr_gks\manage.py runserver 127.0.0.100:8000
